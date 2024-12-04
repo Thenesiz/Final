97b1ee233cc22fb762c34859f5029e4bfbb2787e
Thenesiz <christianjodeldizon@gmail.com>
2024-12-04 14:10:24 +0800

    Delete Infomanage directory

--- Infomanage/test_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'feedbackdb.php';
-echo "Database connection successful!";
-?>
--- Infomanage/test_submit.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "Form submitted! Feedback: " . htmlspecialchars($_POST['feedback']);
-} else {
-    echo "Form not submitted.";
-}
-?>
